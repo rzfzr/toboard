b196ff06e8164dcfc39b9166a0175723faf860ad
Rafael Goncalves <capitao.rafa@gmail.com>
2021-07-19 23:40:57 -0300

    Adding tall progress bars

--- src/components/Progress.tsx
+++ src/components/Progress.tsx
@@ -1,4 +1,25 @@
-function Progress() { }
+import { withStyles } from '@material-ui/core/styles';
+import LinearProgress from '@material-ui/core/LinearProgress';
+
+const BorderLinearProgress = withStyles((theme) => ({
+    root: {
+        height: 50,
+        borderRadius: 5,
+    },
+    colorPrimary: {
+        backgroundColor: theme.palette.grey[theme.palette.type === 'light' ? 200 : 700],
+    },
+    bar: {
+        borderRadius: 5,
+        backgroundColor: '#1a90ff',
+    },
+}))(LinearProgress);
+
+function Progress() {
+    return (
+        <BorderLinearProgress variant="buffer" value={40} valueBuffer={60} />
+    )
+}
 
 
 
--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Progress from './components/Progress'
 
 ReactDOM.render(
   <React.StrictMode>
+    <Progress />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
