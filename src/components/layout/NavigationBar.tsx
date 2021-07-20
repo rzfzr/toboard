3ea2c46b21875d8a3c88e3da67d030ef854887d9
Rafael Goncalves <capitao.rafa@gmail.com>
2021-07-20 18:01:46 -0300

    Scoped CSS

--- src/components/layout/NavigationBar.tsx
+++ src/components/layout/NavigationBar.tsx
@@ -1,7 +1,8 @@
 import { Link } from 'react-router-dom'
+import classes from './NavigationBar.module.css'
 
 export default function NavigationBar() {
-    return <header>
+    return <header className={classes.header}>
         <div>toboard</div>
         <nav>
             <ul>
