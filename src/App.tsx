5b2657fe992f579fbef236cf8d67f5b09cd35192
Rafael Goncalves <capitao.rafa@gmail.com>
2021-08-29 20:54:51 -0300

    Adding material-ui icons, messing with css

--- src/App.tsx
+++ src/App.tsx
@@ -7,16 +7,18 @@ import GoalsPage from './pages/Goals'
 
 export default function App() {
   return (
-    <div>
-      <NavigationBar />
-      <Switch>
-        <Route path='/' exact>
-          <FavoritesPage />
-        </Route>
-        <Route path='/goals' >
-          <GoalsPage />
-        </Route>
-      </Switch>
+    <div className="App">
+      <header className="App-header">
+        <NavigationBar />
+        <Switch>
+          <Route path='/' exact>
+            <FavoritesPage />
+          </Route>
+          <Route path='/goals' >
+            <GoalsPage />
+          </Route>
+        </Switch>
+      </header>
     </div>
   );
 }
--- src/components/Toggle.tsx
+++ src/components/Toggle.tsx
@@ -1,5 +1,6 @@
 import { Button } from '@material-ui/core';
-
+import PlayCircleFilledIcon from '@material-ui/icons/PlayCircleFilled';
+import PauseCircleFilledIcon from '@material-ui/icons/PauseCircleFilled';
 interface Prop {
     color: string;
     isRunning: boolean;
@@ -11,7 +12,9 @@ const ToggleFunction = () => {
 export default function Toggle(props: Prop) {
     const message = props.isRunning ? 'Stop' : 'Play';
     return (
-        <Button onClick={ToggleFunction} variant="contained">{message}
+        <Button
+            startIcon={<PlayCircleFilledIcon />}
+            variant="contained" color="primary" onClick={ToggleFunction}>{message}
         </Button>
     )
 }
