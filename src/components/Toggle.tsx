c9bf7804a3b8ed6c4a0bf9d68b9d2fc4432edf4a
Rafael Goncalves <capitao.rafa@gmail.com>
2021-08-31 11:44:49 -0300

    Using state for toggle button

--- src/components/Toggle.tsx
+++ src/components/Toggle.tsx
@@ -1,21 +1,49 @@
+import React from 'react';
 import { Button } from '@material-ui/core';
 import PlayCircleFilledIcon from '@material-ui/icons/PlayCircleFilled';
 import PauseCircleFilledIcon from '@material-ui/icons/PauseCircleFilled';
-
 interface Prop {
     color: string;
     isRunning: boolean;
 }
 
-const ToggleFunction = () => {
-    alert('toggled')
+function toggle(isRunning: boolean) {
+    console.log('toggling', isRunning)
+    return !isRunning
+
+    // entryDescription, projectID) {
+    // console.log('Togglying: ', entryDescription, projectID);
+    // this.$toggl.getCurrentTimeEntry((err, timeEntry) => {
+    //     if (err) console.log(err);
+    //     else {
+    //         if (timeEntry) {
+    //             console.log("Something already running: ", timeEntry.description, timeEntry.pid);
+    //             console.log("Checking if it is:", entryDescription, projectID);
+    //             if (timeEntry.pid == projectID && timeEntry.description == entryDescription) {
+    //                 console.log('Matched! Stopping');
+    //                 this.stopEntry(timeEntry.id)
+    //             } else {
+    //                 console.log('Not Matched! Starting new');
+    //                 this.createEntry(entryDescription, projectID);
+    //             }
+    //         } else {
+    //             console.log("Nothing running! Starting new");
+    //             this.createEntry(entryDescription, projectID);
+    //         }
+    //     }
+    // });
 }
 export default function Toggle(props: Prop) {
-    const message = props.isRunning ? 'Stop' : 'Play';
+    const [isRunning, setRunning] = React.useState(false);
+
+
+    const status = isRunning ? 'Stop' : 'Play';
+    const handleClick = () => { setRunning(toggle(isRunning)) }
+
     return (
         <Button
-            startIcon={<PlayCircleFilledIcon />}
-            variant="contained" color="primary" onClick={ToggleFunction}>{message}
+            startIcon={isRunning ? <PauseCircleFilledIcon /> : <PlayCircleFilledIcon />}
+            variant="contained" color="primary" onClick={handleClick}>{status}
         </Button>
     )
 }
