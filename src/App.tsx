00d52a532ffeb808a84cb51b1d1b6d25f35adf4b
Rafael Goncalves <capitao.rafa@gmail.com>
2021-08-31 14:14:10 -0300

    Adding api calls

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,73 @@ import FavoritesPage from './pages/Favorites'
 import WeeklyPage from './pages/Weekly'
 import HomePage from './pages/Home';
 
+import TogglClient from "toggl-api";
+const togglClient = new TogglClient({
+  apiToken: process.env.REACT_APP_TOGGL_API
+});
+
+
+function getPreviousMonday() {
+  var date = new Date();
+  var day = date.getDay();
+  var prevMonday = new Date();
+  if (date.getDay() == 0) {
+    prevMonday.setDate(date.getDate() - 7);
+  } else {
+    prevMonday.setDate(date.getDate() - (day - 1));
+  }
+  prevMonday.setHours(0, 0, 0, 0);
+  return prevMonday.toISOString();
+}
+
 export default function App() {
+  function updateProjects(timeEntries: any) {
+    const set = new Set(timeEntries.map((item: { pid: any; }) => item.pid));
+    let uniqueProjects = Array.from(set);
+
+    console.log("updatingProjects", uniqueProjects);
+    uniqueProjects.forEach((entry) => {
+      togglClient.getProjectData(entry, (err: any, projectData: any) => {
+        if (err) {
+          console.log("error: ", err);
+        } else {
+          projectData.sum = 0;
+          timeEntries.forEach((entry: any) => {
+            if (entry.pid == projectData.id) {
+              if (entry.duration > 0) projectData.sum += entry.duration;
+            }
+          });
+          // this.$store.commit("addProject", projectData);
+          // this.setGoals();
+          console.log('GoalSetting?', projectData)
+        }
+      });
+    });
+  }
+
+
+  togglClient.getTimeEntries(
+    getPreviousMonday(),
+    new Date().toISOString(),
+    async (err: any, timeEntries: any) => {
+      if (err) {
+        console.log("error: ", err);
+      } else {
+        console.table(timeEntries)
+        // await this.$store.dispatch("setTimeEntries", timeEntries);
+        // let running = this.$store.state.timeEntries.find(
+        //   (x) => x.duration < 0
+        // );
+        // this.$store.commit("setRunningEntry", running);
+        updateProjects(timeEntries);
+      }
+    }
+  );
+
+
+
+
+
   return (
     <div className="App">
       <header className="App-header">
